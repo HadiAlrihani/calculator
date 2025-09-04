389a4b839705f1e452cf8308f96d8ba08df91c11
HadiAlrihani <hadialrihani@gmail.com>
2025-09-04 15:10:18 -0600

    create files and calculator operation functions

---
+++ calc.js
@@ -0,0 +1,39 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    // num1 and num2 are already floats/doubles
+    return num1 / num2;
+}
+
+let operand1
+let operand2
+let operator
+
+function operate(operand1, operator, operand2) {
+    let result
+    if (operator == '+') {
+        result = add(operand1, operand2); 
+    }
+    else if (operator == '-') {
+        result = subtract(operand1, operand2);
+    }
+    else if (operator == '*') {
+        result = multiply(operand1, operand2);
+    }
+    else if (operator == '/') {
+        result = divide(operand1, operand2);
+    }
+
+    return result;
+
+}
